e759bc490291bb0ff7c03c545c56ae540285fa6e
victor-murta <vmurtag16@gmail.com>
2021-04-01 18:43:14 -0300

    iniciando base do jogo

---
+++ pygameTemplate.py
@@ -0,0 +1,14 @@
+# pygame template  = skeleton for a new game projetc
+import pygame
+import random 
+
+width = 360
+height = 480
+fps = 30
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_
+
+
